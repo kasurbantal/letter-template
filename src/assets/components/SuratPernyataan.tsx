e6c514946a0c096f73a7ea23fbd9b8fa0d7dcfb1
kasurbantal <kasurbantal880@gmail.com>
2025-02-09 14:42:39 +0700

    feat: add Side Bar

--- src/assets/components/SuratPernyataan.tsx
+++ src/assets/components/SuratPernyataan.tsx
@@ -5,6 +5,7 @@ import { saveAs } from "file-saver";
 
 interface FormData {
   nama: string;
+  tempatLahir: string;
   ttl: string;
   noKtp: string;
   alamat: string;
@@ -33,6 +34,7 @@ interface FormData {
 
 const defaultValues: FormData = {
   nama: "",
+  tempatLahir: "",
   ttl: "",
   noKtp: "",
   alamat: "",
@@ -59,32 +61,29 @@ const defaultValues: FormData = {
   tanggal: "",
 };
 
-interface InputField {
-  label: string;
-  type: string;
-  placeholder?: string;
-  options?: string[];
-}
-
-const inputObject: Record<keyof FormData, InputField> = {
-  nama: { label: "Nama", type: "text", placeholder: "Masukkan nama" },
-  ttl: { label: "Tempat, Tanggal Lahir", type: "date" },
+const inputObject: Record<
+  string,
+  { label: string; type: string; options?: string[] }
+> = {
+  nama: { label: "Nama", type: "text" },
+  tempatLahir: { label: "Tempat Lahir", type: "text" },
+  ttl: { label: "Tanggal Lahir", type: "date" },
   noKtp: { label: "No. KTP", type: "text" },
   alamat: { label: "Alamat", type: "text" },
   jalan: { label: "Jalan", type: "text" },
-  rt: { label: "RT", type: "number" },
-  rw: { label: "RW", type: "number" },
-  desa: { label: "Desa", type: "text" },
+  rt: { label: "RT", type: "text" },
+  rw: { label: "RW", type: "text" },
+  desa: { label: "Desa / Kelurahan", type: "text" },
   kecamatan: { label: "Kecamatan", type: "text" },
   kabupaten: { label: "Kabupaten", type: "text" },
   nib: { label: "NIB", type: "text" },
   luas: { label: "Luas", type: "number" },
   statusTanah: { label: "Status Tanah", type: "text" },
-  penggunaan: { label: "Penggunaan", type: "text" },
-  batasUtara: { label: "Sebelah Utara", type: "text" },
-  batasTimur: { label: "Sebelah Timur", type: "text" },
-  batasSelatan: { label: "Sebelah Selatan", type: "text" },
-  batasBarat: { label: "Sebelah Barat", type: "text" },
+  penggunaan: { label: "Dipergunakan Untuk", type: "text" },
+  batasUtara: { label: "Batas Utara", type: "text" },
+  batasTimur: { label: "Batas Timur", type: "text" },
+  batasSelatan: { label: "Batas Selatan", type: "text" },
+  batasBarat: { label: "Batas Barat", type: "text" },
   tahun: { label: "Tahun Dokumen", type: "year" },
   proses: {
     label: "Proses",
@@ -109,11 +108,9 @@ export default function SuratPernyataan() {
     try {
       const response = await fetch("/SuratPernyataan.docx");
       const arrayBuffer = await response.arrayBuffer();
-
       const zip = new PizZip(arrayBuffer);
       const doc = new Docxtemplater(zip);
       doc.render(data);
-
       const output = doc.getZip().generate({ type: "blob" });
       saveAs(output, "Surat-Pernyataan.docx");
     } catch (error) {
@@ -122,73 +119,86 @@ export default function SuratPernyataan() {
   };
 
   return (
-    <div className="min-h-screen py-4 px-4 sm:px-6 lg:px-8 mx-auto bg-white p-8 rounded-lg shadow-lg">
-      <h1 className="font-base text-center mb-8 text-blue-600">
-        Generate Surat Pernyataan
-      </h1>
-      <form onSubmit={handleSubmit(generateDocx)} className="space-y-4">
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-          {Object.entries(inputObject).map(([name, field]) => (
-            <div
-              key={name}
-              className={`${
-                name === "proses" ||
-                name === "nama" ||
-                name === "alamat" ||
-                name === "alamatSaksi1" ||
-                name === "alamatSaksi2"
-                  ? "col-span-2"
-                  : "w-full"
-              }`}
-            >
-              <label className="block text-sm font-medium text-gray-700">
-                {field.label}:
-              </label>
-              {field.type === "select" ? (
-                <select
-                  {...register(name as keyof FormData, {
-                    required: `${field.label} harus diisi`,
-                  })}
-                  className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                >
-                  {field.options?.map((option) => (
-                    <option key={option} value={option}>
-                      {option || "Pilih Proses"}
-                    </option>
-                  ))}
-                </select>
-              ) : (
-                <input
-                  type={field.type}
-                  placeholder={field.placeholder}
-                  {...register(name as keyof FormData, {
-                    required:
-                      name !== "rt" &&
-                      name !== "rw" &&
-                      name !== "nib" &&
-                      name !== "jalan"
-                        ? `${field.label} harus diisi`
-                        : false,
-                  })}
-                  className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                />
-              )}
-              {errors[name as keyof FormData] && (
-                <p className="text-sm text-red-500 mt-1">
-                  {errors[name as keyof FormData]?.message}
-                </p>
-              )}
-            </div>
-          ))}
-        </div>
+    <div className="flex min-h-screen bg-gray-100">
+      {/* Sidebar */}
+      <div className="w-64 bg-white shadow-lg p-6">
+        <h2 className="text-xl font-bold mb-4">Surat Pernyataan</h2>
+        <ul className="space-y-2">
+          <li className="text-gray-400 font-medium cursor-pointer">
+            Surat Pernyataan
+          </li>
+          <li className="text-gray-400 font-medium cursor-pointer">
+            Surat Pernyataan Lainnya
+          </li>
+        </ul>
+      </div>
 
-        <button
-          type="submit"
-          className="w-full text-blue-600 py-2 px-4 rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
-        >
-          Generate Docx
-        </button>
-      </form>
+      {/* Form Section */}
+      <div className="flex-1 p-8 bg-white  rounded-lg shadow-md">
+        <h1 className="text-2xl font-semibold text-center mb-8">
+          Surat Pernyataan Penguasaan Fisik Tanah
+        </h1>
+        <form onSubmit={handleSubmit(generateDocx)} className="space-y-6">
+          <div className="grid grid-cols-2 md:grid-cols-2 gap-4">
+            {Object.entries(inputObject).map(([name, field]) => (
+              <div
+                key={name}
+                className={`${
+                  name === "nama" ||
+                  name === "alamat" ||
+                  name === "alamatSaksi1" ||
+                  name === "alamatSaksi2"
+                    ? "col-span-2"
+                    : "w-full"
+                }`}
+              >
+                <label className="block text-sm font-medium text-gray-700 mb-1">
+                  {field.label}:
+                </label>
+                {field.type === "select" ? (
+                  <select
+                    {...register(name as keyof FormData, {
+                      required: `${field.label} harus diisi`,
+                    })}
+                    className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  >
+                    {field.options?.map((option) => (
+                      <option key={option} value={option}>
+                        {option || "Pilih Proses"}
+                      </option>
+                    ))}
+                  </select>
+                ) : (
+                  <input
+                    type={field.type}
+                    {...register(name as keyof FormData, {
+                      required:
+                        name !== "rt" &&
+                        name !== "rw" &&
+                        name !== "nib" &&
+                        name !== "jalan"
+                          ? `${field.label} harus diisi`
+                          : false,
+                    })}
+                    className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  />
+                )}
+                {errors[name as keyof FormData] && (
+                  <p className="text-sm text-red-500 mt-1">
+                    {errors[name as keyof FormData]?.message}
+                  </p>
+                )}
+              </div>
+            ))}
+          </div>
+          <button
+            type="submit"
+            className="text-blue-500 py-2 px-4 rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
+          >
+            Generate Docx
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
