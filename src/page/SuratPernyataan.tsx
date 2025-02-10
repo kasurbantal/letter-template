1b9525dc07d7478b800e0d2849788db4c352cdc6
kasurbantal <kasurbantal880@gmail.com>
2025-02-10 22:58:47 +0700

    refactor: import FormulirCustom from "../assets/components/FormulirCustom"

--- src/page/SuratPernyataan.tsx
+++ src/page/SuratPernyataan.tsx
@@ -1,11 +1,11 @@
 import { SideBar } from "../assets/components/SideBar";
 import { Formulir } from "../assets/components/Formulir";
-import FormulirCustom from "../assets/components/FormulirCustom1";
+import FormulirCustom from "../assets/components/FormulirCustom";
 
 export default function SuratPernyataan() {
   return (
     <div className="flex min-h-screen bg-gray-100">
-      <SideBar />
+      {/* <SideBar /> */}
       {/* <Formulir /> */}
       <FormulirCustom />
     </div>
